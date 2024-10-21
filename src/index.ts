4020238105592b869b850415c38a989176925a62
Andre <Andre.loewen@usu.com>
2024-10-21 15:52:30 +0200

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,98 @@
-import Cloudflare, { ClientOptions } from "cloudflare";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-if (!(process.env.API_ZONE_ID && process.env.API_KEY && process.env.API_MAIL)) {
-  throw new Error(
-    "'API_ZONE_ID', 'API_KEY' and 'API_EMAIL' must be set in .env"
-  );
+const API_KEY = process.env.API_KEY;
+const API_EMAIL = process.env.API_EMAIL;
+const API_ZONE_ID = process.env.API_ZONE_ID;
+const API_BASE_URL = `https://api.cloudflare.com/client/v4/zones/${API_ZONE_ID}/`;
+
+if (!API_KEY || !API_EMAIL || !API_ZONE_ID) {
+  throw new Error("Necessary configuration not set");
 }
 
-const zone_id = process.env.API_ZONE_ID;
+interface ARecord {
+  id: string;
+  zone_id: string;
+  zone_name: string;
+  name: string;
+  type: "A" | "AAAA";
+  content: string;
+  proxiable: boolean;
+  proxied: boolean;
+  ttl: number;
+  settings: any;
+  meta: {
+    auto_added: boolean;
+    managed_by_app: boolean;
+    managed_by_argo_tunnel: boolean;
+  };
+  comment: string | null;
+  tags: string[];
+  created_on: string;
+  modified_on: string;
+}
 
-const options: ClientOptions = {
-  apiKey: process.env.API_KEY,
-  apiEmail: process.env.API_MAIL
+const getExistingRecords = async (): Promise<ARecord[]> => {
+  const response = await fetch(API_BASE_URL + "dns_records", {
+    headers: {
+      "X-Auth-Key": API_KEY,
+      "X-Auth-Email": API_EMAIL
+    }
+  });
+  const data = await response.json();
+
+  return data.result as ARecord[];
 };
 
-async function getPublicIPAddress(): Promise<string> {
-  try {
-    const response = await fetch("https://api.ipify.org?format=json");
-    if (!response.ok) {
-      throw new Error("Failed to fetch IP address");
-    }
-    const data: any = await response.json();
-    return data.ip;
-  } catch (error) {
-    console.error(error);
-    return "";
-  }
-}
+const getIPv4Address = async (): Promise<string> => {
+  const response = await fetch("https://api.ipify.org?format=json");
+  return (await response.json()).ip;
+};
 
-async function main() {
-  const cf = new Cloudflare(options);
+const getNecessaryRecords = async (): Promise<Partial<ARecord>[]> => {
+  const ip = await getIPv4Address();
 
-  const address = await getPublicIPAddress();
+  // server-kicker.de
+  const base: Partial<ARecord> = {
+    content: ip,
+    type: "A",
+    name: "server-kicker.de"
+  };
 
-  if (!address) {
-    throw new Error("Could not get the public IP address");
-  }
+  const www: Partial<ARecord> = {
+    content: ip,
+    type: "A",
+    name: "www.server-kicker.de"
+  };
 
-  cf.dns.records
-    .list({ zone_id })
-    .then((response) => {
-      response.result.forEach((record) => {
-        if (!record.id || !record.zone_id) {
-          throw new Error("Error while fetching DNS records");
-        }
+  return [base, www];
+};
 
-        const params: Cloudflare.DNS.Records.RecordUpdateParams.DNSRecordsARecord =
-          {
-            zone_id,
-            content: address,
-            name: record.name,
-            type: "A"
-          };
+const updateRecords = async (): Promise<void> => {
+  const existingRecords = await getExistingRecords();
+  const necessaryRecords = await getNecessaryRecords();
 
-        cf.dns.records.update(record.id, params);
-      });
-    })
-    .catch((err) => console.error(err));
-}
+  necessaryRecords.forEach((record) => {
+    let recordId;
+    existingRecords.forEach((eRecord) => {
+      if (record.name === eRecord.name && record.type === eRecord.type) {
+        recordId = eRecord.id;
+        record.id = recordId;
+      }
+    });
 
-main();
+    fetch(API_BASE_URL + "dns_records", {
+      method: recordId ? "PUT" : "POST",
+      headers: {
+        "X-Auth-Key": API_KEY,
+        "X-Auth-Email": API_EMAIL
+      },
+      body: JSON.stringify(record)
+    }).catch((err) => {
+      console.error(err);
+    });
+  });
+};
+
+updateRecords();
